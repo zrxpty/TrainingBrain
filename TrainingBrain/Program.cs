8f766bdd6e356306f34485f30441e7eb8270c511
n.kopylov@data-platform.ru <n.kopylov@data-platform.ru>
2025-08-17 19:24:19 +0700

    пупу

--- TrainingBrain/Program.cs
+++ TrainingBrain/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TrainingBrain.services;
 
-Console.WriteLine("Hello, World!");
+var counter = new SafeCounter();
+
+foreach (var item in Enumerable.Range(1, 1000))
+{
+    var thread = new Thread((o) =>
+        ExecuteSafeCounter(counter));
+    
+    thread.Start();
+}
+
+counter.GetValue();
+
+
+void ExecuteSafeCounter(SafeCounter counter)
+{
+    counter.Increment();
+    
+    Console.WriteLine($"айди потока {Thread.CurrentThread.ManagedThreadId} " +
+                      $"число {counter.GetValue()}" +
+                      $"");
+}
---
+++ TrainingBrain/services/SafeCounter.cs
@@ -0,0 +1,27 @@
+﻿namespace TrainingBrain.services;
+
+public class SafeCounter
+{
+      private int Value { get; set; } = 0;
+      private object locker = new object();
+
+      public void Increment()
+      {
+            lock (locker)
+                  Value++;
+      }
+
+      public void Decrement()
+      {
+            lock (locker)
+                  Value--;
+      }
+
+      public int GetValue()
+      {
+            lock (locker)
+            {
+                  return Value;
+            }
+      }
+}
