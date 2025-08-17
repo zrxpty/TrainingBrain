c1b9697c6d5a8adf70b2aa7942359e5bc81e6370
nikit <nikit@gmail.com>
2025-08-17 21:46:25 +0700

    вызов всех тем

--- TrainingBrain/Program.cs
+++ TrainingBrain/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TrainingBrain.Articles;
 
-Console.WriteLine("Hello, World!");
+var tasks = typeof(ArticleBase).Assembly.GetTypes()
+    .Where(t => typeof(ArticleBase).IsAssignableFrom(t) && !t.IsAbstract)
+    .Select(t => (ArticleBase)Activator.CreateInstance(t)!)
+    .ToList();
+
+Console.WriteLine($"Найдено задач: {tasks.Count}\n");
+
+foreach (var task in tasks)
+{
+    Console.WriteLine($"=== {task.Name} ===");
+    task.Run();
+    Console.WriteLine();
+}
+
+Console.WriteLine("Все задачи выполнены!");
