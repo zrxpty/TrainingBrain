de5398e99471d11299d0b0f08b13c00cf0b7b0d4
nikit <nikit@gmail.com>
2025-08-17 21:45:58 +0700

    добавил тему

---
+++ TrainingBrain/Articles/TestArticle.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingBrain.Articles;
+
+public class TestArticle : ArticleBase
+{
+    public override string Name  => "TestArticle";
+    
+    public override void Run()
+    {
+        Console.WriteLine("Выполняюсь");
+    }
+}
