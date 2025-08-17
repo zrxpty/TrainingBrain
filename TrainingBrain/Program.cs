c3e4108e1ba11e28240bd671572d4becdbfa57e9
nikit <nikit@gmail.com>
2025-08-17 16:58:00 +0700

    йое

--- TrainingBrain/Program.cs
+++ TrainingBrain/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int cnt = 0;
+var list = Enumerable.Range(0, 10000).ToList();
+var options = new ParallelOptions() {MaxDegreeOfParallelism = 8};
+Parallel.ForEach(list, options, (element) =>
+{
 
-Console.WriteLine("Hello, World!");
+    /*var localCnt = Interlocked.Increment(ref cnt);
+    if(localCnt!= cnt)*/
+    var localCnt = 0;    
+    localCnt++;    
+    Console.WriteLine("{0} {1} {2}",cnt, localCnt, Thread.CurrentThread.ManagedThreadId);
+});
